2baeb49aa9dabac77be6ef18e5a69b7b25004185
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-11-10 02:48:49 +0100

    imp: Use "longer" version as "last" for tests to see more interesting stuff.

--- src/tests.cc
+++ src/tests.cc
@@ -6,10 +6,12 @@
 //
 
 int main(int argc, char const *argv[]) {
-    cout << "TESTS: MAIN(get_process_usage())" << endl;
-    cout << Kvmpro::get_process_usage(0) << endl;
 
     cout << "TESTS: MAIN(get_process_usage_short())" << endl;
     cout << Kvmpro::get_process_usage_short(0) << endl;
+
+    cout << "TESTS: MAIN(get_process_usage())" << endl;
+    cout << Kvmpro::get_process_usage(0) << endl;
+
     return 0;
 }
