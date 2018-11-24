33348cc17d96defb7ea224d097f2c976d9991a60
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-11-24 18:16:12 +0100

    new: There's no point in waiting or printing anything from tests_leak.cc

--- src/tests_leak.cc
+++ src/tests_leak.cc
@@ -7,16 +7,12 @@
 
 int main(int argc, char const *argv[]) {
 
-    cout << "TESTS-LEAK: MAIN(get_process_usage_short())" << endl;
     for (int i = 0; i < 10000; i++) {
         get_process_usage_short(0);
-        usleep(100);
     }
 
-    cout << "TESTS-LEAK: MAIN(get_process_usage())" << endl;
-    for (int i = 0; i < 10000; i++) {
+    for (int j = 0; j < 10000; j++) {
         get_process_usage(0);
-        usleep(100);
     }
 
     return 0;
