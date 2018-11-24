64c5d80a86330b77a5705af0a380e76518c68c34
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-11-24 19:41:31 +0100

    new: Single simple loop is enough here.

--- src/tests_leak.cc
+++ src/tests_leak.cc
@@ -7,11 +7,8 @@
 
 int main(int argc, char const *argv[]) {
 
-    for (int i = 0; i < 10000; i++) {
+    for (int i = 0; i < 1000; i++) {
         get_process_usage_short(0);
-    }
-
-    for (int j = 0; j < 10000; j++) {
         get_process_usage(0);
     }
 
