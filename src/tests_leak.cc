d1d6ba2d03b43a644b8c523394beddda8d003701
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-10 20:22:16 +0100

    imp: Refactored tests_leaks.cc to two separate groups x500 calls.

--- src/tests_leak.cc
+++ src/tests_leak.cc
@@ -7,9 +7,12 @@
 
 int main(int argc, char const *argv[]) {
 
-    for (int i = 0; i < 1000; i++) {
-        get_process_usage_short(0);
-        get_process_usage(0);
+    for (int i = 0; i < 500; i++) {
+        get_process_usage(i + 1);
+    }
+
+    for (int j = 0; j < 500; j++) {
+        get_process_usage_short(j + 1);
     }
 
     return 0;
