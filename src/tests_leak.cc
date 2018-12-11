f3a464ec7fe357457b22d95559218b7a71862bf3
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-11 03:30:30 +0100

    new: Avoid using uid 0 for tests_leak and tests

--- src/tests_leak.cc
+++ src/tests_leak.cc
@@ -7,12 +7,12 @@
 
 int main(int argc, char const *argv[]) {
 
-    for (int i = 0; i < 500; i++) {
-        get_process_usage(i + 1);
+    for (int i = 1; i < 500; i++) {
+        get_process_usage(i);
     }
 
-    for (int j = 0; j < 500; j++) {
-        get_process_usage_short(j + 1);
+    for (int j = 1; j < 500; j++) {
+        get_process_usage_short(j);
     }
 
     return 0;
