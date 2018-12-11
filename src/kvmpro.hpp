6f57017d2d1ea0dc24c0a4307d577bd5f2c57571
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-11 16:04:55 +0100

    new: Moved kvmpro.h to kvmpro.hpp

--- src/kvmpro.hpp
+++ src/kvmpro.hpp
@@ -61,7 +61,7 @@
 #elif __NetBSD__
     #error "NetBSD is not supported as production platform."
 #elif __APPLE__
-    //printf "Darwin is not supported as production platform."
+    #error "Darwin is not supported as production platform."
 #else
     #error "Unsupported unknown platform."
 #endif
@@ -72,32 +72,34 @@
 using namespace std;
 
 
-// JSON escape helper for C++:
-const string escape_json(const string& input);
+class Kvmpro {
 
-// convert given protocol to plain string:
-const string protocol_to_string(const int domain, const int type, const int protocol);
+    // Public Functions provided by this library:
+    public:
 
-// convert given address to plain string:
-const string addr_to_string(const struct sockaddr_storage *ss);
+        // JSON escape helper for C++:
+        static const string escape_json(const string& input);
 
-// read procstat data from kernel:
-const string procstat_files(struct procstat *procstat, struct kinfo_proc *kipp); // NOTE: FreeBSD/HardenedBSD specific feature:
+        // convert given protocol to plain string:
+        static const string protocol_to_string(const int domain, const int type, const int protocol);
+
+        // convert given address to plain string:
+        static const string addr_to_string(const struct sockaddr_storage *ss);
+
+        // read procstat data from kernel:
+        static const string procstat_files(struct procstat *procstat, struct kinfo_proc *kipp); // NOTE: FreeBSD/HardenedBSD specific feature:
 
 
-//
-// Exported, Public Functions provided by this library:
-//
+        // generates JSON of processes which belong to user with given "uid".
+        // this list contains every process information available, including open sockets,
+        // open network connections, protocols, bound addresses and ports.
+        static const string get_process_usage(uid_t uid);
 
+        // generates simplified JSON of processes which belong to user with given "uid".
+        // (simplified data structure without "socketstat" field and additional, often rarely used fields).
+        static const string get_process_usage_short(uid_t uid);
 
-// generates JSON of processes which belong to user with given "uid".
-// this list contains every process information available, including open sockets,
-// open network connections, protocols, bound addresses and ports.
-EXPORT_SHARED_OBJECT const string get_process_usage(uid_t uid);
-
-// generates simplified JSON of processes which belong to user with given "uid".
-// (simplified data structure without "socketstat" field and additional, often rarely used fields).
-EXPORT_SHARED_OBJECT const string get_process_usage_short(uid_t uid);
+};
 
 
 #endif
