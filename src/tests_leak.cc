2b227485b2923d2b396af4b6ae769f53e1db7a6b
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-11 16:07:04 +0100

    fix: Missing Kvmpro:: for tests_leak

--- src/tests_leak.cc
+++ src/tests_leak.cc
@@ -8,11 +8,11 @@
 int main(int argc, char const *argv[]) {
 
     for (int i = 1; i < 500; i++) {
-        get_process_usage(i);
+        Kvmpro::get_process_usage(i);
     }
 
     for (int j = 1; j < 500; j++) {
-        get_process_usage_short(j);
+        Kvmpro::get_process_usage_short(j);
     }
 
     return 0;
