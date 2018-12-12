3dbda631ca20ea752752b411af21009057526a03
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-12 22:52:01 +0100

    new: Extended tests_leak and tests with both function calls (C/C++).

--- src/tests.cc
+++ src/tests.cc
@@ -9,9 +9,11 @@ int main(int argc, char const *argv[]) {
 
     cout << "TESTS: MAIN(get_process_usage_short())" << endl;
     cout << Kvmpro::get_process_usage_short(65) << endl;
+    cout << get_process_usage_short(65) << endl;
 
     cout << "TESTS: MAIN(get_process_usage())" << endl;
     cout << Kvmpro::get_process_usage(65) << endl;
+    cout << get_process_usage(65) << endl;
 
     return 0;
 }
--- src/tests_leak.cc
+++ src/tests_leak.cc
@@ -5,14 +5,29 @@
 // kvmpro - memory leak utility check
 //
 
+const uint ROUNDS = 1024;
+
+
 int main(int argc, char const *argv[]) {
 
-    for (int i = 1; i < 500; i++) {
-        Kvmpro::get_process_usage(i);
+    // C
+    for (int ps1 = 0; ps1 < ROUNDS; ps1++) {
+        get_process_usage(++ps1);
     }
 
-    for (int j = 1; j < 500; j++) {
-        Kvmpro::get_process_usage_short(j);
+    // C++
+    for (int ps2 = 0; ps2 < ROUNDS; ps2++) {
+        Kvmpro::get_process_usage(++ps2);
+    }
+
+    // C
+    for (int ps3 = 1; ps3 < ROUNDS; ps3++) {
+        get_process_usage_short(++ps3);
+    }
+
+    // C++
+    for (int ps4 = 1; ps4 < ROUNDS; ps4++) {
+        Kvmpro::get_process_usage_short(++ps4);
     }
 
     return 0;
