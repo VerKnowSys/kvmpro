40a73f420ebe8fed30e442452f27aa01e0cb27ed
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-13 20:21:04 +0100

    imp: Extended tests. Call a short loop in tests.

--- src/tests.cc
+++ src/tests.cc
@@ -7,13 +7,22 @@
 
 int main(int argc, char const *argv[]) {
 
-    cout << "TESTS: MAIN(get_process_usage_short())" << endl;
+    cout << "TESTS: C++: MAIN(get_process_usage_short())" << endl;
     cout << Kvmpro::get_process_usage_short(65) << endl;
+    cout << "TESTS: C: MAIN(get_process_usage_short())" << endl;
     cout << get_process_usage_short(65) << endl;
 
-    cout << "TESTS: MAIN(get_process_usage())" << endl;
+    cout << "TESTS: C++: MAIN(get_process_usage())" << endl;
     cout << Kvmpro::get_process_usage(65) << endl;
+    cout << "TESTS: C: MAIN(get_process_usage())" << endl;
     cout << get_process_usage(65) << endl;
+
+    for (int i = 0; i < 1000; ++i) {
+        if (i % 100 == 0)
+            cout << "AT: " << i << ", of: " << 1000 << endl;
+        get_process_usage(0);
+        get_process_usage_short(0);
+    }
 
     return 0;
 }
