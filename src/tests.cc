ed1922a623262bd596e94149e885d8b41c571f66
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-17 20:54:14 +0100

    new: Commited case where object after free() is still there…

--- src/tests.cc
+++ src/tests.cc
@@ -7,27 +7,43 @@
 
 int main(int argc, char const *argv[]) {
 
+    uid_t uid = 65;
+
     cout << "TESTS: C++: MAIN(get_process_usage_short())" << endl;
-    cout << Kvmpro::get_process_usage_short(65) << endl;
+    cout << Kvmpro::get_process_usage_short(uid) << endl;
     cout << "TESTS: C: MAIN(get_process_usage_short())" << endl;
-    cout << get_process_usage_short(65) << endl;
+    cout << get_process_usage_short(uid) << endl;
 
     cout << "TESTS: C++: MAIN(get_process_usage())" << endl;
-    cout << Kvmpro::get_process_usage(65) << endl;
+    cout << Kvmpro::get_process_usage(uid) << endl;
     cout << "TESTS: C: MAIN(get_process_usage())" << endl;
-    cout << get_process_usage(65) << endl;
+    cout << get_process_usage(uid) << endl;
+
+    cout << "TESTS: C: MAIN(get_process_usage_tp())" << endl;
+    cout << get_process_usage_tp(uid)->bytes << endl;
+    cout << "TESTS: C: MAIN(get_process_usage_short_tp())" << endl;
+    cout << get_process_usage_short_tp(uid)->bytes << endl;
 
     cout << "TESTS: C: MAIN(get_process_usage_t())" << endl;
-    cout << get_process_usage_t(65).bytes << endl;
+    cout << get_process_usage_t(uid).bytes << endl;
 
     cout << "TESTS: C: MAIN(get_process_usage_short_t())" << endl;
-    cout << get_process_usage_short_t(65).bytes << endl;
+    cout << get_process_usage_short_t(uid).bytes << endl;
 
     for (int i = 0; i < 1000; ++i) {
         if (i % 100 == 0)
             cout << "AT: " << i << ", of: " << 1000 << endl;
-        get_process_usage(0);
-        get_process_usage_short(0);
+
+        kvmpro_t* a = get_process_usage_tp(uid);
+        kvmpro_t* b = get_process_usage_short_tp(uid);
+        assert(a != nullptr);
+        assert(b != nullptr);
+        destroy_kvmpro_tp(a);
+        destroy_kvmpro_tp(b);
+        // assert(a == nullptr); // WHY THESE ARE NOT NULL?!
+        // assert(b == nullptr); // WHY THESE ARE NOT NULL?!
+        get_process_usage_t(uid);
+        get_process_usage_short_t(uid);
     }
 
     return 0;
